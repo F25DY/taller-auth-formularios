65af4ce9368ccfc2b8b1bb4eab4be8eaf01e9e50
Timms <timmsarizafredy2@gmail.com>
2026-08-15 17:13:20 -0500

    Inicializador proyecto sb

---
+++ taller-auth-formularios/src/main/java/com/taller/auth/taller_auth_formularios/TallerAuthFormulariosApplication.java
@@ -0,0 +1,13 @@
+package com.taller.auth.taller_auth_formularios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerAuthFormulariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerAuthFormulariosApplication.class, args);
+	}
+
+}
