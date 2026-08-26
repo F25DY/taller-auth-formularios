211b1ce6521e722c47ae663ccc60e8cd67a11a38
bertelsarasofia-code <bertelsarasofia@gmail.com>
2026-08-25 21:41:40 -0500

    concurrecia

--- taller-auth-formularios/src/main/java/com/taller/auth/taller_auth/security/SecurityConfig.java
+++ taller-auth-formularios/src/main/java/com/taller/auth/taller_auth/security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -46,5 +46,12 @@ public class SecurityConfig {
             );
 
         return http.build();
+    
     }
+    @Bean
+public HttpSessionEventPublisher httpSessionEventPublisher() {
+    return new HttpSessionEventPublisher();
 }
+
+}
+
