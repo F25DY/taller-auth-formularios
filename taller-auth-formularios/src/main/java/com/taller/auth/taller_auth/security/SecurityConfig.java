a38406e5646f3f2c139996af35818bf51400e882
Timms <timmsarizafredy2@gmail.com>
2026-08-25 22:35:12 -0500

    Correccion de errores

--- taller-auth-formularios/src/main/java/com/taller/auth/taller_auth/security/SecurityConfig.java
+++ taller-auth-formularios/src/main/java/com/taller/auth/taller_auth/security/SecurityConfig.java
@@ -52,12 +52,8 @@ public class SecurityConfig {
             );
 
         return http.build();
-    
+
     }
-    @Bean
-public HttpSessionEventPublisher httpSessionEventPublisher() {
-    return new HttpSessionEventPublisher();
-}
 
 }
 
